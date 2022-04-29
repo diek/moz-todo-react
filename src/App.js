a42b4c8f276bb5e4ef77667eb57b83a58ea704f0
Derrick Kearney <d_kearney@bellaliant.net>
2022-04-29 00:18:10 -0300

    Update app to include components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
+import Form from "./components/Form";
+import FilterButton from "./components/FilterButton";
+import Todo from "./components/Todo";
 
 function App(props) {
-  const subject = props.subject;
+
+  function addTask(name) {
+    alert(name);
+  }
+
+  const taskList = props.tasks.map(task => (
+    <Todo
+      id={task.id}
+      key={task.id}
+      name={task.name}
+      completed={task.completed}
+    />
+  )
+  );
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello Django and {subject}!
-        </p>
-      </header>
+    <div className="todoapp stack-large">
+      <h1>TodoMatic</h1>
+      <Form addTask={addTask} />
+      <div className="filters btn-group stack-exception">
+        <FilterButton />
+        <FilterButton />
+        <FilterButton />
+      </div>
+      <h2 id="list-heading">3 Tasks remaining</h2>
+      <ul
+        role="list"
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading"
+      >
+        {taskList}
+      </ul>
     </div>
   );
 }
-
 export default App;
